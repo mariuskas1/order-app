a67a6e31931fc8a22fc0753ab5883d792b1e0892
mariuskas1 <you@example.com>
2024-05-18 12:21:13 +0200

    updated js

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 let names = [];
 let prices = [];
+let amounts = [];
+
+function addToBasket(name, price, amount) {
+  let index = names.indexOf(name);
+  if (index == -1) {
+    names.push(name);
+    prices.push(price);
+    amounts.push(amount);
+  } else {
+    amounts[index]++;
+  }
+    
+    displayShoppingBasket();
+    updateShoppingSum();
+}
 
 
-function addToBasket(name, price) {
-  names.push(name);
-  prices.push(price);
-
+function displayShoppingBasket() {
   let basket = document.getElementById("basket");
   basket.innerHTML = "";
 
   for (let i = 0; i < names.length; i++) {
     const name = names[i];
     const price = prices[i];
+    const amount = amounts[i];
 
     basket.innerHTML += `
         <div class="item">
-            <span class="amount">1x</span>
+            <span class="amount">${amount}x</span>
             <span class="item-name">${name}</span>
             <div>
                 <img class="item-icon amount-change" src="./img/icons/minus-solid.svg">
@@ -26,15 +39,14 @@ function addToBasket(name, price) {
         </div>
     `;
   }
-  updateShoppingBasket();
 }
 
 
-function updateShoppingBasket() {
+function updateShoppingSum() {
   let sum = 0;
 
   for (let i = 0; i < prices.length; i++) {
-    sum += prices[i];
+    sum += prices[i] * amounts[i];
   }
 
   let finalSum = sum + 1;
@@ -42,3 +54,14 @@ function updateShoppingBasket() {
   document.getElementById("sum").innerHTML = sum;
   document.getElementById("finalSum").innerHTML = finalSum;
 }
+
+
+window.onscroll = function () {
+  let shoppingCart = document.getElementById("shoppingCart");
+
+  if (window.scrollY > 0) {
+    shoppingCart.style = "top: 0";
+  } else {
+    shoppingCart.style = "top: 100px";
+  }
+};
